9dd72a7bd4d9f1dae93a71499965843cf3546062
Lewis Coulson <info@lewiscoulson.com>
2015-09-23 18:43:21 +0100

    added bin

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
 var fs = require('fs');
 
-var componentName = process.argv[2];
-var destination = process.argv[3] + componentName + '/';
+module.exports = function() {
+	var componentName = process.argv[2];
+	var destination = process.argv[3] + componentName + '/';
 
-var htmlFilePath = destination + componentName + '.html';
-var htmlfileComment = '<!-- ' + componentName + ' component -->';
+	var htmlFilePath = destination + componentName + '.html';
+	var htmlfileComment = '<!-- ' + componentName + ' component -->';
 
-var sassFilePath = destination + componentName + '.scss';
-var sassfileComment = '// ' + componentName + ' component';
+	var sassFilePath = destination + componentName + '.scss';
+	var sassfileComment = '// ' + componentName + ' component';
 
-var jsFilePath = destination + componentName + '.js';
-var jsfileComment = '// ' + componentName + ' component';
+	var jsFilePath = destination + componentName + '.js';
+	var jsfileComment = '// ' + componentName + ' component';
 
-var testFolderPath = destination + 'test/';
-var testFilePath = testFolderPath + componentName + '_test.js';
+	var testFolderPath = destination + 'test/';
+	var testFilePath = testFolderPath + componentName + '_test.js';
 
-fs.mkdir(destination, function(error) {
-  if (error) throw error;
-
-  fs.writeFile(htmlFilePath, htmlfileComment, function (error) {
-	  if (error) throw error;
-	  console.log('html file saved');
-	});
-
-  fs.writeFile(sassFilePath, sassfileComment, function (error) {
-	  if (error) throw error;
-	  console.log('sass file saved');
-	});
-
-	fs.writeFile(jsFilePath, jsfileComment, function (error) {
-	  if (error) throw error;
-	  console.log('js file saved');
-	});
-
-	fs.mkdir(testFolderPath, function(error) {
+	fs.mkdir(destination, function(error) {
 	  if (error) throw error;
 
-	  fs.writeFile(testFilePath, '', function (error) {
+	  fs.writeFile(htmlFilePath, htmlfileComment, function (error) {
 		  if (error) throw error;
-		  console.log('test file saved');
+		  console.log('html file saved');
 		});
-	});
+
+	  fs.writeFile(sassFilePath, sassfileComment, function (error) {
+		  if (error) throw error;
+		  console.log('sass file saved');
+		});
+
+		fs.writeFile(jsFilePath, jsfileComment, function (error) {
+		  if (error) throw error;
+		  console.log('js file saved');
+		});
+
+		fs.mkdir(testFolderPath, function(error) {
+		  if (error) throw error;
+
+		  fs.writeFile(testFilePath, '', function (error) {
+			  if (error) throw error;
+			  console.log('test file saved');
+			});
+		});
+	}
 });
